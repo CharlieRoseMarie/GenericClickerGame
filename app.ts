89205b13ab6ffe2fe3a2d191b61d4235b0c29bb8
Charlotte Rose-Marie Allen <CharlotteRMAllen@gmail.com>
2018-05-31 00:41:35 -0700

    Converted to typescript

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import * as express from "express";
+import * as express from 'express';
 import {SplashController} from "./routes";
 
 const app : express.Application() = express();
@@ -7,5 +7,5 @@ const port : number = process.env.PORT || 3000;
 app.use('/splash', SplashController);
 app.listen(port, () =>
 {
-  console.log("Listening at http://localhost:${port}");
-}
+  console.log('Listening at http://localhost:${port}');
+});
